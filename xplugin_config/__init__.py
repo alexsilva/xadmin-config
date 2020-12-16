547555e5a83eb06b309c46b79a9b53b97e593b4a
alex <geniofuturo@gmail.com>
2020-12-16 14:05:52 -0300

    start plugin.

---
+++ xplugin_config/__init__.py
@@ -0,0 +1,17 @@
+from django.template.loader import render_to_string
+from xadmin.plugins.utils import get_context_dict
+from xadmin.views import BaseAdminPlugin
+
+
+class ConfigPlugin(BaseAdminPlugin):
+    """Configuration plugin for other plugins"""
+    component_config = True
+
+    def init_request(self, *args, **kwargs):
+        return self.component_config
+
+    def block_top_toolbar(self, context, nodes):
+        """toobar"""
+        context = get_context_dict(context)
+        nodes.append(render_to_string("xplugin_config/blocks/comm.top.config.html",
+                                      context=context))
