73ac6fdc4a3a765ad513e22e5bd77d6e9b2ba384
alex <alex@fabricadigital.com.br>
2021-01-08 18:20:10 -0300

    Add default opts to get.

--- xplugin_config/static/xplugin_config/js/storage.js
+++ xplugin_config/static/xplugin_config/js/storage.js
@@ -7,8 +7,8 @@ jQuery(function ($) {
         this.namespace = name + ".";
         return this;
     }
-    Storage.prototype.get = function (key) {
-        return $.getCookie(this.namespace + key);
+    Storage.prototype.get = function (key, opts) {
+        return $.getCookie(this.namespace + key) || opts;
     }
     Storage.prototype.set = function (key, value) {
         $.setCookie(this.namespace + key, value);
