dcfd7e14ce015470de1dc55ead11021da2d1b425
alexsilva <geniofuturo@gmail.com>
2024-09-25 14:42:12 -0300

    v1.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='xadmin-config',
-    version='1.0',
+    version='1.1.0',
     packages=['xplugin_config'],
     url='https://github.com/alexsilva/xadmin-config',
     license='MIT',
